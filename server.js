82bb485e705b9da8cda75db5a0ef5df6bd510fdf
mateus.luz <mateus.luz@gsw.com.br>
2020-07-08 17:10:03 -0300

    include /player route

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
+console.log(__dirname)
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/player', (req, res) => {
+    res.send('Hello Player!')
 })
 
 app.listen(port, () => {
